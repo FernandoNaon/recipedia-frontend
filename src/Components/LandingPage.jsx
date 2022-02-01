f7876a57b8720d865427add48b8c3bdd5219c7de
Fernando Naon <74456529+FernandoNaon@users.noreply.github.com>
2022-01-31 23:56:58 -0300

    restyled landing page

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./LandingPage.module.css";
+// import gif from "../Assets/Ingredients.gif";
 
 const LandingPage = () => {
   return (
-    <div className={styles.bg}>
-      <div className={styles.titleContainer}>
-        <h1 className={styles.title}>Recipedia</h1>
-        <div className={styles.btncontainer}>
-          <Link to="/home">
-            <button className={styles.lndbtn}>Start</button>
-          </Link>
+    <div className={styles.landingContainer}>
+      <div className={styles.bg}>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>Recipedia</h1>
+          {/* <img src={gif} alt="gif" className={styles.gif} /> */}
+          <div className={styles.btncontainer}>
+            <Link to="/home">
+              <button className={styles.lndbtn}>Start</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
